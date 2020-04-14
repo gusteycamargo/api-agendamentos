843767d7304447c7373c0cb8cd857ad5ca3fee56
gustaacamargo <camargo25.gustavo@gmail.com>
2020-04-13 23:02:50 -0300

    Corrige bugs

--- app/Controllers/Http/ScheduleController.js
+++ app/Controllers/Http/ScheduleController.js
@@ -82,12 +82,16 @@ class ScheduleController {
 
             const { user, place, course, category, equipamentsName, date, addressee } = await retrieveDataToEmailConfirmation(equipaments, schedule);
             
-            await Mail.send('emails.confirmationSchedule', { schedule, date, user, equipamentsName, place, course, category }, (message) => {
-              message
-                  .from('donotreplyagendamento@unespar.edu.br')
-                  .to(addressee.email)
-                  .subject('Confirmação de agendamento')
-            });
+            try {
+              await Mail.send('emails.confirmationSchedule', { schedule, date, user, equipamentsName, place, course, category }, (message) => {
+                message
+                    .from('donotreplyagendamento@unespar.edu.br')
+                    .to(addressee.email)
+                    .subject('Confirmação de agendamento')
+              });
+            } catch (error) {
+              console.log(error);
+            }
 
             return schedule;
           }
